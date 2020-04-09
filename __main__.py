a80f2244d1aebd863b73a391784c7c6a52e5e8b9
gigamarr <nofacemarr@gmail.com>
2020-04-09 18:31:55 +0400

    added ability to choose number of results to return

--- __main__.py
+++ __main__.py
@@ -4,10 +4,15 @@ import argparse
 from smartsnek import Word
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Search definition(s) of a word.")
     parser.add_argument('word', help='search definition of this word')
+    parser.add_argument('-q', '--quantity', type=int, help='number of results to return')
     args = parser.parse_args()
     word = Word(args.word)
+    if args.quantity:
+        word = Word(args.word, quantity=args.quantity)
+    else:
+        word = Word(args.word)
     print(word)
 
 if __name__ == "__main__":
